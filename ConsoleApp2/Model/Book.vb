3a8f0b3d40f6ae72b0e3877749a644a937580660
ddjwajww <127021017+ddjwajww@users.noreply.github.com>
2023-12-18 07:58:26 +0300

    Delete Book.vb

--- ConsoleApp2/Model/Book.vb
+++ ConsoleApp2/Model/Book.vb
@@ -1,18 +1,1 @@
-﻿Imports infrastructure
 
-Public Class Book
-    Implements IEntity
-
-    Public Property BookId As Integer
-    Public Property Title As String
-    Public Property AuthorId As Integer?
-    Public Property CategoryId As Integer?
-    Public Property PublishDate As Date?
-    Public Property AvailableCopies As Integer?
-    Public Property TotalCopies As Integer?
-    Public Property PicturePath As String
-    Public Property IsDeleted As Boolean
-    Public Property Author As Author
-    Public Property Category As Category
-
-End Class
