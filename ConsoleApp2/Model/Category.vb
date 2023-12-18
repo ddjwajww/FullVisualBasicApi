36e47996f4bdbc39f31f6cb32a07ccbb5f65e080
ddjwajww <127021017+ddjwajww@users.noreply.github.com>
2023-12-18 07:59:53 +0300

    Delete Category.vb

--- ConsoleApp2/Model/Category.vb
+++ ConsoleApp2/Model/Category.vb
@@ -1,13 +1,1 @@
-﻿Imports infrastructure
 
-Public Class Category
-    Implements IEntity
-
-    Public Property CategoryId As Integer
-    Public Property CategoryName As String
-    Public Property Description As String
-    Public Property PicturePath As String
-    Public Property IsDeleted As Boolean
-    Public Property Books As List(Of Book)
-
-End Class
