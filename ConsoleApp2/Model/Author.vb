a65a8e45ba8bae6f1bed1c34d8930f79b497b429
ddjwajww <127021017+ddjwajww@users.noreply.github.com>
2023-12-18 07:58:06 +0300

    Delete Author.vb

--- ConsoleApp2/Model/Author.vb
+++ ConsoleApp2/Model/Author.vb
@@ -1,16 +1,1 @@
-﻿Imports System.Security.Principal
-Imports infrastructure
-
-
-Public Class Author
-    Implements IEntity
-
-    Public Property AuthorId As Integer
-    Public Property AuthorName As String
-    Public Property BirthDate As Date
-    Public Property PicturePath As String
-    Public Property IsDeleted As Boolean
-    Public Property Books As List(Of Book)
-
-End Class
 
