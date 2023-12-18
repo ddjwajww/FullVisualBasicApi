87a586739de839e7dcd01eba027eb1332e7a9816
ddjwajww <127021017+ddjwajww@users.noreply.github.com>
2023-12-18 07:59:28 +0300

    Delete Reservation.vb

--- ConsoleApp2/Model/Reservation.vb
+++ ConsoleApp2/Model/Reservation.vb
@@ -1,17 +1,1 @@
-﻿Imports infrastructure
-
-Public Class Reservation
-    Implements IEntity
-
-    Public Property ReservationId As Integer
-    Public Property UserId As Integer?
-    Public Property BookId As Integer?
-    Public Property ReservationDate As Date?
-    Public Property ExpirationDate As Date?
-    Public Property Active As Boolean
-    Public Property IsDeleted As Boolean
-    Public Property User As User
-    Public Property Book As Book
-
-End Class
 
